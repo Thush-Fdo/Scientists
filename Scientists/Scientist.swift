b89385898678d4d30f74137d814a1d675c00c9d8
Thush-Fdo <tsherminfdo@gmail.com>
2024-06-20 13:58:00 +0530

    Refactored. Add Scientists pdf.

---
+++ Scientists/Scientist.swift
@@ -0,0 +1,17 @@
+//
+//  Scientist.swift
+//  SpotTheScientist
+//
+//  Created by 101 on 2022/10/4.
+//
+
+import Foundation
+
+struct Scientist: Decodable{
+    let name: String
+    let dates: String
+    let field: String
+    let bio: String
+    let country: String
+    let source: String
+}
